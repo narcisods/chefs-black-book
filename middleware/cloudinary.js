5adf4a731a441f3629295c955cbb7cb316b96b81
Narciso Salvador <narcisodsalvador@gmail.com>
2022-10-11 12:05:28 -0700

    updated clludinary config

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -3,6 +3,7 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({
+  cloudinary_url: process.env.CLOUDINARY_URL,
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
