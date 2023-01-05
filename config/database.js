d67ec68b88becea7cc89a06eef7e9706967bd201
Narciso Salvador <narcisodsalvador@gmail.com>
2023-01-04 16:55:51 -0800

    added tostring

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.DB_STRING, {});
+		const conn = await mongoose.connect(process.env.DB_STRING.toString(), {});
 
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (err) {
